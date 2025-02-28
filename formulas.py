d7025db2a2ff18ee03a732385ab33151076a0347
Jeremy <jer.auriel@gmail.com>
2025-02-28 10:57:08 +0700

    initial commit

---
+++ formulas.py
@@ -0,0 +1,13 @@
+class formula:
+    def add(a,b):
+        return a+b
+    def subtract(a,b):
+        return a-b
+    def divide(a,b):
+        return a/b
+    def multi(a,b):
+        return a*b
+    def mod(a,b):
+        return a%b
+    def power(a,b):
+        return a**b   
---
+++ main.py
@@ -0,0 +1,10 @@
+from formulas import formula
+    
+
+def main():
+
+    print("hasil :", formula.add(10,10))
+
+
+if __name__ == "__main__":
+    main()
